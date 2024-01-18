2656a3f95583f9b03cbdde8e83fbba94a94eead8
Afrizal Dwi Handika <142969775+Gracetav@users.noreply.github.com>
2024-01-18 11:36:58 +0700

    Update latihan2.php

--- latihan2.php
+++ latihan2.php
@@ -1,21 +1,18 @@
 <?php
+function Angkot($noAngkot,  $kondisiTerminasi, $kondisiTerminasi1, $kondisiTerminasi2, $kondisiTerminasi3,$kondisiTerminasi31,$kondisiTerminasi32, $kondisiTerminasi4, $kondisiTerminasi5,$kondisiTerminasi6, $kondisiTerminasi7){
 
-function cek_adatidaknya_angkot($noAngkot) {
-    if ($noAngkot == 2 || $noAngkot == 9 || $noAngkot == 12 || $noAngkot == 17) {
-        return "sedang diperbaiki";
-    } elseif ($noAngkot == 10 || $noAngkot == 11) {
-        return "tersedia";
-    } elseif ($noAngkot >= 4 && $noAngkot <= 20) {
-        return "tidak tersedia";
-    } else {
-        return "tersedia";
+for($noAngkot; $noAngkot <= $kondisiTerminasi; $noAngkot++){
+    if($noAngkot <= $kondisiTerminasi1 || $noAngkot == $kondisiTerminasi3 || $noAngkot == $kondisiTerminasi31 ||$noAngkot == $kondisiTerminasi32 ){
+        echo "Angkot no - $noAngkot tersedia <br/>";
+    }else if($noAngkot <= $kondisiTerminasi2 || $noAngkot == $kondisiTerminasi5 || $noAngkot == $kondisiTerminasi6 || $noAngkot == $kondisiTerminasi7 ){
+        echo "Angkot no - $noAngkot sedang diperbaiki<br/>";
     }
-}
 
-echo "=== Program Angkot Sederhana ===<br/>\n";
-for ($no = 1; $no <= 20; $no++) {
-    $status = cek_adatidaknya_angkot($no);
-    echo "Angkot no - $no $status\n<br/>"; 
-}
+    else{
+        echo "Angkot no - $noAngkot tidak tersedia <br/>";
+    }
 
-?>  
+}
+}
+echo Angkot(1, 20, 1, 2,3,10,11, 4,9,12,17);
+?>
